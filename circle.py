fc36e3e9969214c3b79f62e61a6ebd19ccb5243d
Danny Mendoza <dmendoza@pop-os.localdomain>
2020-09-10 19:31:48 -0700

    completed problem

--- circle.py
+++ circle.py
@@ -6,23 +6,33 @@ class Cirlce:
     
     def __repr__(self):
         return f"Circle({self.radius})"
+    
+    @property
+    def radius(self):
+        return self._radius
+
+        
     @property
     def diameter(self):
         return self.radius * 2
-    @property    
+    @property
     def area(self):
         return (self.radius**2)*math.pi
 
+
+    @diameter.setter
+    def diameter(self, diameter):
+        self.radius = diameter / 2
     
-    
-    
+    @radius.setter
+    def radius(self, radius):
+        if radius < 0:
+            raise ValueError("Radius cannot be negative")
+        self._radius = radius
 
 
 
 
-c = Cirlce(4)
-c.radius=1
-
-print(c.area)
-print(c.diameter)
-print(c)
+c = Cirlce(1)
+c.diameter=4
+print(c.radius)
